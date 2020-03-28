8901dafbb99f5a1459a70716853bbc4d79569902
Christian Miller <christian.david.miller@gmail.com>
2020-03-28 16:24:54 -0700

    move js to own file + rename to index.html

---
+++ resume/scripts/main.js
@@ -0,0 +1,29 @@
+     $(".jobs").hide();
+	$(".paid").on("click", function(){
+	    $(".jobs").show();
+	});
+	$(".paid").on("dblclick", function(){
+	    $(".jobs").hide();
+	});
+     $(".volt").hide();
+	$(".unPaid").on("click", function(){
+	    $(".volt").show();
+	});
+	$(".unPaid").on("dblclick", function(){
+	    $(".volt").hide();
+	});
+     $(".refs").hide();
+	$(".tellem").on("click", function(){
+	    $(".refs").show();
+	});
+	$(".tellem").on("dblclick", function(){
+	    $(".refs").hide();
+	});
+     $(".knows").hide();
+	$(".learnt").on("click", function(){
+	    $(".knows").show();
+	});
+	$(".learnt").on("dblclick", function(){
+	    $(".knows").hide();
+	});
+     
