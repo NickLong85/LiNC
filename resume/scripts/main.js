689538d5579412cba2ffce92aeedd81e972dcdc9
NickLong85 <62810257+NickLong85@users.noreply.github.com>
2020-03-28 20:06:57 -0700

    main2.js
    Updated functionality to include all text content from html

--- resume/scripts/main.js
+++ resume/scripts/main.js
@@ -1,29 +1,105 @@
-     $(".jobs").hide();
-	$(".paid").on("click", function(){
-	    $(".jobs").show();
-	});
-	$(".paid").on("dblclick", function(){
-	    $(".jobs").hide();
-	});
-     $(".volt").hide();
-	$(".unPaid").on("click", function(){
-	    $(".volt").show();
-	});
-	$(".unPaid").on("dblclick", function(){
-	    $(".volt").hide();
-	});
-     $(".refs").hide();
-	$(".tellem").on("click", function(){
-	    $(".refs").show();
-	});
-	$(".tellem").on("dblclick", function(){
-	    $(".refs").hide();
-	});
-     $(".knows").hide();
-	$(".learnt").on("click", function(){
-	    $(".knows").show();
-	});
-	$(".learnt").on("dblclick", function(){
-	    $(".knows").hide();
-	});
-     
+	     $(".jobs").hide();
+	        $(".paid").on("click", function(){
+	            $(".jobs").show();
+	        });
+	        $(".paid").on("dblclick", function(){
+	            $(".jobs").hide();
+	        });
+	     $(".volt").hide();
+	        $(".unPaid").on("click", function(){
+	            $(".volt").show();
+	        });
+	        $(".unPaid").on("dblclick", function(){
+	            $(".volt").hide();
+	        });
+	     $(".refs").hide();
+	        $(".tellem").on("click", function(){
+	            $(".refs").show();
+	        });
+	        $(".tellem").on("dblclick", function(){
+	            $(".refs").hide();
+	        });
+	     $(".knows").hide();
+	        $(".learnt").on("click", function(){
+	            $(".knows").show();
+	        });
+	        $(".learnt").on("dblclick", function(){
+	            $(".knows").hide();
+	        });
+	     $(".cook").hide();
+	        $(".fud").on("click", function(){
+	            $(".cook").show();
+	        });
+	        $(".fud").on("dblclick", function(){
+	            $(".cook").hide();
+	        });
+	        $(".proder").hide();
+	        $(".airWaves").on("click", function(){
+	            $(".proder").show();
+	        });
+	        $(".airWaves").on("dblclick", function(){
+	            $(".proder").hide();
+	        });
+	        $(".teach").hide();
+	        $(".liluns").on("click", function(){
+	            $(".teach").show();
+	        });
+	        $(".liluns").on("dblclick", function(){
+	            $(".teach").hide();
+	        });
+	        $(".vegs").hide();
+	        $(".growem").on("click", function(){
+	            $(".vegs").show();
+	        });
+	        $(".growem").on("dblclick", function(){
+	            $(".vegs").hide();
+	        });
+	        $(".eros").hide();
+	        $(".artWork").on("click", function(){
+	            $(".eros").show();
+	        });
+	        $(".artWork").on("dblclick", function(){
+	            $(".eros").hide();
+	        });
+	        $(".coach").hide();
+	        $(".olymps").on("click", function(){
+	            $(".coach").show();
+	        });
+	        $(".olymps").on("dblclick", function(){
+	            $(".coach").hide();
+	        });
+	        $(".frankie").hide();
+	        $(".Sophia").on("click", function(){
+	            $(".frankie").show();
+	        });
+	        $(".Sophia").on("dblclick", function(){
+	            $(".frankie").hide();
+	        });
+	        $(".toms").hide();
+	        $(".Mike").on("click", function(){
+	            $(".toms").show();
+	        });
+	        $(".Mike").on("dblclick", function(){
+	            $(".toms").hide();
+	        });
+	        $(".chiefs").hide();
+	        $(".chefs").on("click", function(){
+	            $(".chiefs").show();
+	        });
+	        $(".chefs").on("dblclick", function(){
+	            $(".chiefs").hide();
+	        });
+	        $(".deg").hide();
+	        $(".learn").on("click", function(){
+	            $(".deg").show();
+	        });
+	        $(".learn").on("dblclick", function(){
+	            $(".deg").hide();
+	        });
+	        $(".KHAN").hide();
+	        $(".cud").on("click", function(){
+	            $(".KHAN").show();
+	        });
+	        $(".cud").on("dblclick", function(){
+	            $(".KHAN").hide();
+	        });
